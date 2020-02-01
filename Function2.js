0fcc33e50470760af08d3f3a949b861100c5d896
Feroz-Khan-Coder <Feroz.FK007@gmail.com>
2020-02-01 12:22:09 +0530

    Saveit all

---
+++ Function2.js
@@ -0,0 +1,13 @@
+/*var x= function(a,b)// addition
+{
+    return a+b;
+}
+var z=x(10,20);
+console.log(z);*/
+/*function multiply(a,b)// multiply
+{
+    return a*b;
+}
+var x=multiply(10,20);
+console.log(x);*/
+var myfunction=new Function("a","b",return a*b);
---
+++ array/array5.js
@@ -0,0 +1,6 @@
+var x= function(a,b)
+{
+    return a+b;
+}
+var z=x(10,20);
+console.log(z);
