ecc3682bf86050ee6a09d1241f869450376ec5a0
Feroz-Khan-Coder <Feroz.FK007@gmail.com>
2020-02-01 11:42:24 +0530

    added new 2 files
    added new files

---
+++ Area.js
@@ -0,0 +1,5 @@
+
+var radius=20;
+var pi=3.14;
+var area=radius*radius*pi
+console.log("Area of Circle " + area);
---
+++ Array.js
@@ -0,0 +1,12 @@
+var arr=[10,20,30,40,50];
+//console.log(arr);
+//console.log(arr[1]);
+var arr1=["car","bus","van","bycycle",79];//
+var arr2=new Array(10,20,30,40,50);//
+console.log(arr);//
+console.log(arr1);//
+console.log(arr.length);//
+console.log(arr2);//
+console.log(arr[0]=60);//
+console.log(arr);//   
+var obj=(first:"First",second:"second",third:"Third", age:50);
